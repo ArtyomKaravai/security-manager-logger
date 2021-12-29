f69c813305c53c74cf6735089d30bcf570c3510a
a2.karavay <paramonix613@gmail.com>
2021-12-29 18:54:23 +0300

    add RabbitMq

---
+++ src/main/java/com/itransition/securitymanagerlogger/SecurityManagerLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.itransition.securitymanagerlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityManagerLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityManagerLoggerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itransition/securitymanagerlogger/SecurityManagerLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itransition.securitymanagerlogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityManagerLoggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
