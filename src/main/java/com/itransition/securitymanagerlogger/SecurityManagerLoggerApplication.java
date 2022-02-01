c0c03d37d2c1d724169064724eac06591083af33
a2.karavay <paramonix613@gmail.com>
2022-02-01 14:07:18 +0300

    edit SecurityManagerLoggerApplication

--- src/main/java/com/itransition/securitymanagerlogger/SecurityManagerLoggerApplication.java
+++ src/main/java/com/itransition/securitymanagerlogger/SecurityManagerLoggerApplication.java
@@ -1,9 +1,7 @@
 package com.itransition.securitymanagerlogger;
 
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SecurityManagerLoggerApplication {
